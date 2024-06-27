af445bb27f782307761c6fec934a73c18977952d
Mark <markbaes6996@gmail.com>
2024-06-27 03:34:39 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{ env('APP_NAME') }}</title>
+    </head>
+
+    <body>
+        @if (false)
+            <h1>Hello</h1>
+        @else
+            <h1>Hello</h1>
+        @endif
+
+    </body>
+
+</html>
